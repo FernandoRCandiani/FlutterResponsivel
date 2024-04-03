3c06b20eaeb2fdaea642baf563c21d473060471d
Fernando Candiani <candianirfernando@gmail.com>
2024-04-02 22:21:21 -0300

    Projeto Flutter Responsivo

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/responsividade_media_query.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: "Flutter Web",
+    debugShowCheckedModeBanner: false,
+    home: ReaponsividadeMediaQuery(),
+  ));
+}
+
---
+++ lib/responsividade_media_query.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ReaponsividadeMediaQuery extends StatefulWidget {
+  const ReaponsividadeMediaQuery({super.key});
+
+  @override
+  State<ReaponsividadeMediaQuery> createState() => _ReaponsividadeMediaQueryState();
+}
+
+class _ReaponsividadeMediaQueryState extends State<ReaponsividadeMediaQuery> {
+
+  @override
+  Widget build(BuildContext context) {
+
+  double largura = MediaQuery.of(context).size.width;
+  double altura = MediaQuery.of(context).size.height;
+  double alturaBarraStatus = MediaQuery.of(context).padding.top;
+  double alturaAppBar = AppBar().preferredSize.height;
+  double larguraItem = largura / 3;
+
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Responsividade'),
+      ),
+      body: Row(
+        children: [
+          Container(
+            width: larguraItem,
+            height: altura - alturaAppBar - alturaBarraStatus,
+            color: Colors.red,
+          ),
+          Container(
+            width: larguraItem,
+            height: altura - alturaAppBar - alturaBarraStatus,
+            color: Colors.black,
+          ),
+          Container(
+            width: larguraItem,
+            height: altura - alturaAppBar - alturaBarraStatus,
+            color: Colors.yellow,
+          )
+        ],
+      ),
+    );
+  }
+}
