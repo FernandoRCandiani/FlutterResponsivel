648c4243e2101bbc48ade750b1901a6efcda98c6
Fernando Candiani <candianirfernando@gmail.com>
2024-04-02 22:47:46 -0300

    Trabalhando com Column, Row e Expanded

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/responsividade_media_query.dart';
+import 'package:flutter_application_1/responsividade_row_col.dart';
 
 void main() {
   runApp(const MaterialApp(
     title: "Flutter Web",
     debugShowCheckedModeBanner: false,
-    home: ReaponsividadeMediaQuery(),
+    home: ReaponsividadeRowCol(),
   ));
 }
 
---
+++ lib/responsividade_row_col.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ReaponsividadeRowCol extends StatefulWidget {
+  const ReaponsividadeRowCol({super.key});
+
+  @override
+  State<ReaponsividadeRowCol> createState() => _ReaponsividadeRowColState();
+}
+
+class _ReaponsividadeRowColState extends State<ReaponsividadeRowCol> {
+  @override
+  Widget build(BuildContext context) {
+    double largura = MediaQuery.of(context).size.width;
+    double altura = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Responsividade'),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                flex: 3,
+                child: Container(
+                  height: 200,
+                  color: Colors.amber,
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  height: 200,
+                  color: Colors.orange,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  height: 200,
+                  color: Colors.red,
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    height: 200,
+                    color: Colors.black,
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    height: 200,
+                    color: Colors.green,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    height: 200,
+                    color: Colors.blue,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
