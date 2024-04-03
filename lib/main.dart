ea52d4e441c33c2f3391c0db3658aa6b07891f1f
Fernando Candiani <candianirfernando@gmail.com>
2024-04-03 20:14:29 -0300

    Trabalhando OrientationBuilder e GridView

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/responsividade_wrap.dart';
+import 'package:flutter_application_1/orientacao.dart';
 
 void main() {
   runApp(const MaterialApp(
     title: "Flutter Web",
     debugShowCheckedModeBanner: false,
-    home: ResponsividadeWrap(),
+    home: Orientacao(),
   ));
 }
 
